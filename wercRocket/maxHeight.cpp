5e7bff58132fea16079ac81ba78f78af7c794d22
krowclif <krowclif@uwo.ca>
2017-02-08 20:27:54 -0500

    Update maxHeight.cpp

--- wercRocket/maxHeight.cpp
+++ wercRocket/maxHeight.cpp
@@ -1,4 +1,11 @@
 
 
 #include "maxHeight.h"
-maxHeight(currentHeight,estAirDensity,estCoeffDrag,currentSpeed){}
+maxHeight(currentHeight,estAirDensity,estCoeffDrag,currentSpeed){
+  const double dryMass=10;//dry mass in Kg
+  const double thereGoesGravity=9.8;
+  const double crossArea=0.014637068;//cross section area in m^2
+  double terminalVelocitySquared=(2*dryMass*thereGoesGravity/(estCoeffDrag*crossArea*estAirDensity));//simplified math by not taking the square root here
+  double maxHeight=terminalVelocitySquared/(2*thereGoesGravity)*log((pow(currentSpeed,2)+terminalVelocitySquared)/terminalVelocitySquared)+currentHeight;
+  return maxHeight;
+}
