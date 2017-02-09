261e24d2dde0f569b03f1d1ce9c9d694ad7ffd23
darcese <darcese@uwo.ca>
2017-02-08 19:25:30 -0500

    skeleton pseudocode

---
+++ wercRocket/wercRocket.ino
@@ -0,0 +1,14 @@
+void setup(){
+}
+
+void loop(){
+}
+
+estCoeffDrag(currentSpeed);
+estAirDensity(currentHeight,groundPressure);
+maxHeight(currentHeight,estAirDensity,estCoeffDrag,currentSpeed);
+targetHeight(currentHeight);
+if(maxHeight()>targetHeight())
+  //more break
+else
+  //less break
